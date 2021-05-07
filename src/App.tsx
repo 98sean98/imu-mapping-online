e977bf0c79b7431ec85228029ce9bad92181d110
seanchok <seanchok@gmail.com>
2021-05-07 18:14:16 +0800

    build side effect to unzip the uploaded file into a lot of files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import JSZip from 'jszip';
 
 import { DragAndDropUpload } from 'components/DragAndDropUpload';
 
 import { AppContext } from 'utilities/context';
 
+type UnzippedFile = {
+  name: string;
+  date: Date;
+  content: string;
+};
+
 function App() {
   const [zipFile, setZipFile] = useState<File>();
+  const [unzippedFiles, setUnzippedFiles] = useState<Array<UnzippedFile>>([]);
+
+  useEffect(() => {
+    if (typeof zipFile !== 'undefined') {
+      const zip = new JSZip();
+      zip.loadAsync(zipFile).then(async (unzipped) => {
+        const newUnzippedFiles: Array<UnzippedFile> = await Promise.all(
+          Object.values(unzipped.files).map(async (file) => {
+            const fileContent = await file.async('text');
+            return {
+              name: file.name,
+              date: file.date,
+              content: fileContent,
+            };
+          }),
+        );
+        setUnzippedFiles(newUnzippedFiles);
+      });
+    }
+  }, [zipFile]);
+
+  useEffect(() => {
+    console.log({ unzippedFiles });
+  }, [unzippedFiles]);
 
   return (
     <AppContext.Provider value={{ zipFile, setZipFile }}>
